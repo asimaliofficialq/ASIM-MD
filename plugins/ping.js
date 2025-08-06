b973ec7932bd0c40dea7d43e076ca5aa8011f384
asimaliofficialq <awaseem9001@gmail.com>
2025-08-06 21:41:56 +0500

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -41,7 +41,7 @@ async (conn, mek, m, { from, quoted, sender, reply }) => {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363399579001828@g.us',
+                    newsletterJid: '120363344550191646@newsletter',
                     newsletterName: "ASIM-MD",
                     serverMessageId: 143
                 }
